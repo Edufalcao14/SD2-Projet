64bcc7f4c72c6829d6df41396f538fab0b62d7a2
Pepijn Smeding <pepijn.smeding@student.vinci.be>
2024-02-26 12:23:43 +0100

    change: Villes

--- src/Villes.java
+++ src/Villes.java
@@ -2,15 +2,23 @@ import java.util.Objects;
 
 public class Villes {
 
+        private int idVille;
         private String nom;
 
-        public Villes(String nom) {
+        private int longitude;
+
+        private int latitude;
+
+        public Villes(int idVille, String nom, int longitude, int latitude) {
+            this.idVille = idVille;
             this.nom = nom;
+            this.longitude = longitude;
+            this.latitude = latitude;
         }
 
         @Override
         public String toString() {
-            return nom;
+            return "idVille:" + idVille + ", nom:" + nom + ", longitude:" + longitude + ", latitude:" + latitude;
         }
 
         @Override
