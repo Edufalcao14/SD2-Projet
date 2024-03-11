9143e07e2240bb8f50b262cae82ca1840a2bc553
Tom Simonis <tom.simonis@student.vinci.be>
2024-03-11 11:25:17 +0100

    add: graph constructor

--- src/Graph.java
+++ src/Graph.java
@@ -3,12 +3,13 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class Graph {
 
-  private Map<Ville, Set<Route>> mapRoutes; // map des villes -> routes entrantes/sortantes
+  private Map<Ville, Set<Ville>> mapRoutes; // map des villes -> villes connectées
   private Map<String, Ville> mapVilles; // map pour recuperer une ville sur base d'une string
   private Map<Integer, Ville> mapIdVilles; // map pour recuperer une ville sur base de son id
 
@@ -17,7 +18,6 @@ public class Graph {
     BufferedReader brcities = new BufferedReader(frcities);
     FileReader frRoads = new FileReader(roads);
     BufferedReader brRoads = new BufferedReader(frRoads);
-    mapRoutes = new HashMap<>();
     mapVilles = new HashMap<>();
     mapIdVilles = new HashMap<>();
     String str;
@@ -32,35 +32,27 @@ public class Graph {
       double longitude = Double.parseDouble(tabVilles[3]);
       Ville ville = new Ville(id, nom, longitude, latitude);
       mapIdVilles.put(id, ville);
-      mapVilles.put(nom,ville);
+      mapVilles.put(nom, ville);
     }
 
-    while ((str = brRoads.readLine()) != null){
+    mapRoutes = new HashMap<>();
+    for (Ville ville : mapVilles.values()) {
+      mapRoutes.put(ville, new HashSet<>());
+    }
+
+    while ((str = brRoads.readLine()) != null) {
       tabRoutes = str.split(",");
       int idVilleDepart = Integer.parseInt(tabRoutes[0]);
       Ville villeDepart = mapIdVilles.get(idVilleDepart);
       int idVilleArrivee = Integer.parseInt(tabRoutes[1]);
-      Ville villeArrivee = mapIdVilles.get(idVilleDepart);
+      Ville villeArrivee = mapIdVilles.get(idVilleArrivee);
       Route route = new Route(villeDepart, villeArrivee, Util.distance(villeArrivee.getLatitude(),
           villeArrivee.getLongitude(), villeDepart.getLatitude(), villeDepart.getLongitude()));
+      mapRoutes.get(villeDepart).add(villeArrivee);
+      mapRoutes.get(villeArrivee).add(villeDepart); // Pour l'unidirection des routes
     }
-   /*
-    while ((st = brTroncons.readLine()) != null) {
-      tabTroncons = st.split(",");
-      Station depart = new Station(tabTroncons[1]);
-      Station arrivee = new Station(tabTroncons[2]);
-      mapStations.put(tabTroncons[1], depart);
-      mapStations.put(tabTroncons[2], arrivee);
-      Troncon troncon = new Troncon(Integer.parseInt(tabTroncons[0]), depart, arrivee,
-          Integer.parseInt(tabTroncons[3]));
-      if (!mapTroncons.containsKey(depart)) {
-        mapTroncons.put(depart, new HashSet<>());
-      }
-      mapTroncons.get(depart).add(troncon);
-
-    }
-
-     */
   }
+
+
 
 }
